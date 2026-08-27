7db25e565829f4c18a3b69854bfe7fc0c1838b7f
l060122jun-dotcom <l060122jun-dotcom@users.noreply.github.com>
2026-08-27 10:42:41 +0800

    feat: customize daily class periods

--- MiniProgram/pages/settings/settings.js
+++ MiniProgram/pages/settings/settings.js
@@ -1,8 +1,15 @@
 const store = require('../../utils/store')
 const reminder = require('../../utils/reminder-adapter')
 Page({
-  data: { document: {}, reminderMessage: '' },
-  onShow() { this.setData({ document: store.load() }) },
+  data: { document: {}, reminderMessage: '', countOptions: [], periodCountIndex: 0, periods: [] },
+  onShow() { const document = store.load(); const periods = document.periods || []; this.setData({ document, periods, countOptions: Array.from({ length: 14 }, (_, i) => String(i + 1)), periodCountIndex: Math.max(0, periods.length - 1) }) },
+  periodCountChange(event) {
+    const count = Number(event.detail.value) + 1; const periods = this.data.periods.slice();
+    while (periods.length < count) { const index = periods.length + 1; periods.push({ index, start: '08:00', end: '08:45' }) }
+    this.setData({ periods: periods.slice(0, count), periodCountIndex: count - 1 })
+  },
+  periodTimeChange(event) { const periods = this.data.periods.slice(); const index = Number(event.currentTarget.dataset.index); periods[index][event.currentTarget.dataset.key] = event.detail.value; this.setData({ periods }) },
+  savePeriods() { const document = store.load(); document.periods = this.data.periods.map((period, index) => ({ index: index + 1, start: period.start, end: period.end })); store.save(document); this.setData({ document, periods: document.periods }); wx.showToast({ title: '时间设置已保存', icon: 'success' }) },
   subscribe() {
     this.setData({ reminderMessage: '正在请求订阅权限…' })
     reminder.requestSubscription()
